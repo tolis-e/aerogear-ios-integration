6d1305208b0bef0226f9dfef6779d8a0b79f5504
Christos Vasilakis <cvasilak@gmail.com>
2013-12-05 13:01:35 +0200

    convert to an application target

---
+++ AeroGear-iOS-Integration/AeroGear-iOS-Integration/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AeroGear-iOS-Integration
+//
+//  Created by Christos Vasilakis on 12/4/13.
+//  Copyright (c) 2013 JBoss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AGAppDelegate class]));
+    }
+}
